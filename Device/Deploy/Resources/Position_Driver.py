71b587a27736e01d78a569d4cb9075236f256686
Nandor Pi2 Work <nandor1992@yahoo.com>
2016-11-23 11:39:23 +0000

    modified:   Device/Deploy/Resources/Database_Driver.py 	new file:   Device/Deploy/Resources/Metadata_Driver.py 	new file:   Device/Deploy/Resources/Position_Driver.py 	modified:   Device/config.ini

--- Device/Deploy/Resources/Position_Driver.py
+++ Device/Deploy/Resources/Position_Driver.py
@@ -6,7 +6,7 @@ import time
 import os,sys
 import ConfigParser
 import datetime
-import couchdb
+## This is Dummy might need to make it smarter later on 
 #Config Settings
 Config=ConfigParser.ConfigParser()
 Config.read(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))+"/config.ini")
@@ -20,42 +20,16 @@ connection = pika.BlockingConnection(parameters);
 channel = connection.channel()
 channel.basic_qos(prefetch_count=1)
 
-
-map_databases = '''function(doc){
-        emit(doc.datetime,doc.payload);
-        }'''
-couch=couchdb.Server('http://'+Config.get("couchDB","user")+':'+Config.get("couchDB","pass")+'@127.0.0.1:5984/')
-
-def getData(db,first,last):
-    resp=""
-    print ("Data: "+first+" "+last)
-    look=db.query(map_databases)
-    for p in look[first:last]:
-        print(p.value)
-        resp=resp+p.value+";"
-    return resp
         
 def on_request(ch, method, properties, body):
     app_name=properties.headers.get("app")
     req_type=properties.headers.get("task")
-    if (req_type=="add"):
-        print("Received Add Message")
-        db=couch["app_"+app_name.lower()]
-        db.save({'datetime':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),'payload':body})
     if (req_type=="get"):
-        print("Received Get Message")
-        db=couch["app_"+app_name.lower()]
-        parts=body.split(';')
-        response=getData(db,parts[0],parts[1])
-        print("Resp: ")
-        print(response)
-        properties_m=pika.BasicProperties(headers={'app':app_name,'res': "storage"})
-        channel.basic_publish(exchange='resource', routing_key='', body=response, properties=properties_m)
+        resp="{'latitude':'52.405259','longitude':'-1.499708','city':'Coventry','country':'United Kingdom'}"
+        properties_m=pika.BasicProperties(headers={'app':app_name,'res': "position"})
+        channel.basic_publish(exchange='resource', routing_key='', body=resp, properties=properties_m)
     ch.basic_ack(delivery_tag = method.delivery_tag)
-
-
-    
-channel.basic_consume(on_request, queue=Config.get("couchDB","queue"))
+channel.basic_consume(on_request, queue=Config.get("Position","queue"))
 
 
 print(" [x] Awaiting RPC requests")
